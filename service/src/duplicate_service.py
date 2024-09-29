7de397fc690957253c33db7fe6924a599be3532d
user <denis.ovcharenko.job@gmail.com>
2024-09-29 10:16:47 +0500

    fix

--- service/src/duplicate_service.py
+++ service/src/duplicate_service.py
@@ -1,6 +1,6 @@
 from repository import mark_duplicate,mark_duplicate_test, mark_hard, get_by_id, save_weight
 from faiss_search import search
-from similar_audio import compare_audio
+from similar_audio import (
 from similar2 import compare_videos
 
 ## Интерфейс анализа дубликатов
@@ -15,13 +15,21 @@ def analyze(file_id):
     model_d = get_by_id(model_id)
     file_d = get_by_id(file_id)
 
-    audio_w = compare_audio(file_path, search_file_path)
-    if file_d.created > model_d.created and float(audio_w) > 0.98 and w <= 140:
+    if w <= 100 and file_d.created > model_d.created:
         original_id = model_id
         duplicte_id = file_id
         original_time = 0
         duplicte_time = 0
         mark_duplicate(file_id, True, original_id)
+        ##mark_hard(file_id,)
+    elif 100 < w <= 135 and float(
+            compare_videos(file_path, search_file_path)) > 0.93 and file_d.created > model_d.created:
+        original_id = model_id
+        duplicte_id = file_id
+        original_time = 0
+        duplicte_time = 0
+        mark_duplicate(file_id, True, original_id)
+        ##mark_hard(file_id,)
     else:
         original_id = file_id
         original_time = None
@@ -29,6 +37,6 @@ def analyze(file_id):
         duplicte_time = None
 
 
-    save_weight(file_id, model_id, str(w), str(audio_w))
+    save_weight(file_id, model_id, str(w), None)
 
     return {"origin":{"id": original_id, "time" : original_time},"dupliacte": {"id": duplicte_id, "time" : duplicte_time}}
